6484119d0dfc1513b3bb4b643a0199eaac52fc18
barak obama <barakobama@guy.com>
2024-10-25 02:47:14 +0100

    part 1 live plot faster

--- part1.py
+++ part1.py
@@ -48,14 +48,15 @@ def main():
         # time.sleep(0.1)
 
         icm20948.icm20948_Gyro_Accel_Read()
-        icm20948.icm20948MagRead()
-        icm20948.icm20948CalAvgValue()
-        icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175,MotionVal[2] * 0.0175,
-                    MotionVal[3],MotionVal[4],MotionVal[5],
-                    MotionVal[6], MotionVal[7], MotionVal[8])
-        pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
-        roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
-        yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
+        # icm20948.icm20948MagRead()
+        # icm20948.icm20948CalAvgValue()
+        # icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175,MotionVal[2] * 0.0175,
+         #            MotionVal[3],MotionVal[4],MotionVal[5],
+        #             MotionVal[6], MotionVal[7], MotionVal[8])
+        # pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
+        # roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
+        # yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
+        pitch = roll = yaw = 0
 
         print("\r\n /-------------------------------------------------------------/ \r\n")
 
@@ -78,9 +79,9 @@ def main():
 
         # general measurements:
 
-        print('\r\nRoll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
-        print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))
-        print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
+        # print('\r\nRoll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
+        # print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))
+        # print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
 
         # real-time plot:
         time_vec.append(start_time - beginning_time)
